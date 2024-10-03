75801c89c7c5309c80cfd66d7755b4add4febd48
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-03 21:01:53 +0200

    removed ref from function in useConfirmationDialogStore.ts

--- composables/ui/confirmation-dialog/useConfirmationDialogStore.ts
+++ composables/ui/confirmation-dialog/useConfirmationDialogStore.ts
@@ -2,8 +2,8 @@ export const useConfirmationDialogStore = defineStore('ConfirmationDialogStore',
   const isOpen: Ref<boolean> = ref(false);
   const title: Ref<string> = ref('');
   const message: Ref<string> = ref('');
-  const onConfirm: Ref<() => void> = ref<() => void>(() => {});
-  const onCancel: Ref<() => void> = ref<() => void>(() => {});
+  const onConfirm: () => void = () => {};
+  const onCancel: () => void = () => {};
 
   return {
     isOpen,
