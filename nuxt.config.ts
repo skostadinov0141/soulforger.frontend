3a9f533ed4cb3bebeccaf75f66ef0041f236f031
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-03 20:16:30 +0200

    added pinia persist state

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,12 @@ import { eslintConfig } from './eslint';
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module', '@nuxt/eslint', '@pinia/nuxt'],
+  modules: [
+    'vuetify-nuxt-module',
+    '@nuxt/eslint',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate',
+  ],
   vuetify: {
     vuetifyOptions: vuetifyOptions,
   },
