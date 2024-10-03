ff540bc4c8b91d9c61057e94fc4546938c54feaa
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-03 17:33:41 +0200

    removed server side pagination

--- composables/dtos/attribute-template/search.post.dto.ts
+++ composables/dtos/attribute-template/search.post.dto.ts
@@ -2,8 +2,6 @@ export class SearchAttributeTemplateDto {
   searchString?: string;
   sortOrder: number;
   sortBy: string;
-  limit: number;
-  page: number;
   includeTags?: string[];
   excludeTags?: string[];
   includeGroups?: string[];
