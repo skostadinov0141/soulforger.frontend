4281c9f3a0e0718ea137a9d032ae59ee67ad2293
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-13 22:32:19 +0200

    SFF-9 finished fixedNumericValueCard.vue

--- components/attributes/create/fixedNumericValueCard.vue
+++ components/attributes/create/fixedNumericValueCard.vue
@@ -1,13 +1,21 @@
 <template>
-  <p class="text-medium-emphasis mt-4">
-    Eine einfache Zahl, die nicht berechnet wird. Diese benötigt keine zusätzliche Konfiguration.
-  </p>
+  <v-row>
+    <v-col cols="12">
+      <p class="text-medium-emphasis">
+        Eine einfache Zahl, die nicht berechnet wird. Diese benötigt keine zusätzliche Konfiguration.
+      </p>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
 import type { CreateAttributeFixedNumericValueTemplateDto } from '~/composables/dtos/attribute-template/create.post.dto';
 
-defineModel<CreateAttributeFixedNumericValueTemplateDto>();
+defineModel<CreateAttributeFixedNumericValueTemplateDto>({
+  default: () => ({
+    value: undefined,
+  }),
+});
 </script>
 
 <style scoped>
