d86874f0c0aff81797c3cae8dbf73434221ecac2
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-13 16:50:00 +0200

    SFF-5 Added group and tag selection fields to [:rulebookId].vue

--- pages/attribute-templates/create/[:rulebookId].vue
+++ pages/attribute-templates/create/[:rulebookId].vue
@@ -1,24 +1,63 @@
 <template>
-  <pre>
-    {{ rulebook }}
-  </pre>
-  <pre>
-    {{ attributeTags }}
-  </pre>
-  <pre>
-    {{ attributeGroups }}
-  </pre>
-  <pre>
-    {{ attributePaths }}
-  </pre>
+  <v-container
+    min-height="100vh"
+    fluid
+    class="pa-10"
+  >
+    <v-row>
+      <v-col cols="4">
+        <v-text-field
+          v-model="rulebook!.name"
+          readonly
+          disabled
+          label="Regelwerk"
+        />
+      </v-col>
+      <v-col cols="8">
+        <v-text-field
+          v-model="payload.name"
+          label="Attributname"
+        />
+      </v-col>
+      <v-col cols="6">
+        <v-autocomplete
+          v-model="payload.tags"
+          v-model:search="tagsSearchValue"
+          prepend-inner-icon="mdi-tag"
+          label="Tags"
+          chips
+          multiple
+          item-title="name"
+          return-object
+          :items="attributeTags ?? []"
+          @keydown.enter="createTag(tagsSearchValue)"
+        />
+      </v-col>
+      <v-col cols="6">
+        <v-autocomplete
+          v-model="payload.group"
+          v-model:search="groupSearchValue"
+          prepend-inner-icon="mdi-account-group"
+          label="Gruppe"
+          item-title="name"
+          return-object
+          :items="attributeGroups ?? []"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts">
-import type { CreateAttributeTemplateDto } from '~/composables/dtos/attribute-template/create.post.dto';
+import type {
+  CreateAttributeTag,
+  CreateAttributeTemplateDto,
+} from '~/composables/dtos/attribute-template/create.post.dto';
 
 const rulebookService = useRulebookService();
 const attributeTemplateService = useAttributeTemplateService();
 const route = useRoute();
+const snackbar = useSnackbar();
 
 // <editor-fold desc="Async Data declarations">
 const { data: rulebook } = await useAsyncData(
@@ -53,6 +92,29 @@ const payload: Ref<CreateAttributeTemplateDto> = ref<CreateAttributeTemplateDto>
   attributeType: undefined,
   attributeValue: undefined,
 });
+const tagsSearchValue: Ref<string> = ref('');
+const groupSearchValue: Ref<string> = ref('');
+// </editor-fold>
+
+// <editor-fold desc="Methods">
+function createTag(tag: string) {
+  if (attributeTags.value!.some(t => t.name === tag)) {
+    tagsSearchValue.value = '';
+    return;
+  }
+  // ensure that the tag is not already present
+  if (payload.value.tags.some(t => t.name === tag)) {
+    tagsSearchValue.value = '';
+    snackbar.error('Tag bereits vorhanden oder reserviert');
+    return;
+  }
+  const tagPaload: CreateAttributeTag = {
+    name: tag,
+    for: 'attribute',
+  };
+  payload.value.tags.push(tagPaload);
+  tagsSearchValue.value = '';
+}
 // </editor-fold>
 </script>
 
