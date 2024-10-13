8d54640901d5f6dba3862b6eedf0877a55100db7
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-14 00:26:27 +0200

    SFF-10 added add diceRoll functionality to calculatedNumericValueCard.vue

--- components/attributes/create/calculatedNumericValueCard.vue
+++ components/attributes/create/calculatedNumericValueCard.vue
@@ -5,6 +5,7 @@
         v-model="currentAttribute"
         :items="paths ?? []"
         item-title="name"
+        prepend-inner-icon="mdi-tag"
         return-object
         label="Attribut"
       />
@@ -26,11 +27,110 @@
         @click="addVariable()"
       />
     </v-col>
+    <v-col
+      v-if="model.variables.length > 0"
+      cols="12"
+    >
+      <v-list
+        variant="text"
+        max-height="300px"
+        class="my-0"
+        bg-color="transparent"
+        rounded
+      >
+        <template
+          v-for="(variable, index) in model.variables"
+          :key="`variable-list-item-${index}`"
+        >
+          <v-divider v-if="index !== 0" />
+          <v-list-item>
+            {{ variable.name }}
+            <template #append>
+              <v-btn
+                icon
+                variant="text"
+                density="compact"
+                color="error"
+                @click="removeVariable(index)"
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </template>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-col>
+    <v-col cols="12">
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="currentDiceRoll.name"
+            label="Würfelname"
+            prepend-inner-icon="mdi-tag"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model="currentDiceRoll.diceAmount"
+            label="Anzahl"
+            prepend-inner-icon="mdi-dice-multiple"
+          />
+        </v-col>
+        <v-col class="d-flex align-center">
+          <v-text-field
+            v-model="currentDiceRoll.diceSides"
+            label="Seitenanzahl"
+            prepend-inner-icon="mdi-dice-d20"
+          />
+          <v-btn
+            icon
+            class="ml-6"
+            rounded
+            @click="addDiceRoll()"
+          >
+            <v-icon>mdi-send</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-col>
+    <v-col
+      v-if="model.diceRolls.length > 0"
+      cols="12"
+    >
+      <v-list
+        class="py-0 border-thin"
+        rounded
+        variant="text"
+        max-height="300px"
+        bg-color="transparent"
+      >
+        <template
+          v-for="(diceRoll, index) in model.diceRolls"
+          :key="`dice-roll-list-item-${index}`"
+        >
+          <v-divider v-if="index !== 0" />
+          <v-list-item>
+            {{ diceRoll.name }}
+            <template #append>
+              <v-btn
+                icon="mdi-delete"
+                variant="text"
+                density="compact"
+                color="error"
+                @click="removeDiceRoll(index)"
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </template>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-col>
   </v-row>
 </template>
 
 <script setup lang="ts">
-import type {
+import type { CreateAttributeDiceRollTemplateDto,
   CreateAttributeCalculatedNumericValueTemplateDto,
   CreateAttributeCharacterFieldPathTemplateDto,
 } from '~/composables/dtos/attribute-template/create.post.dto';
@@ -47,11 +147,62 @@ const model = defineModel<CreateAttributeCalculatedNumericValueTemplateDto>({
   required: true,
 });
 
-const props = defineProps<{
+defineProps<{
   paths: PathRegistryEntity[]
 }>();
 
 const currentAttribute = ref<CreateAttributeCharacterFieldPathTemplateDto>();
+const currentDiceRoll = ref<CreateAttributeDiceRollTemplateDto>({
+  name: undefined,
+  diceAmount: undefined,
+  diceSides: undefined,
+});
+
+function removeVariable(index: number) {
+  const newVariables = model.value.variables.filter((_, i) => i !== index);
+  model.value = {
+    ...model.value,
+    variables: newVariables as [],
+  };
+}
+
+function removeDiceRoll(index: number) {
+  const newDiceRolls = model.value.diceRolls.filter((_, i) => i !== index);
+  model.value = {
+    ...model.value,
+    diceRolls: newDiceRolls as [],
+  };
+}
+
+function addDiceRoll() {
+  if (!currentDiceRoll.value.name) {
+    snackbar.error('Wähle einen Namen für den Würfel');
+    return;
+  }
+  if (!currentDiceRoll.value.diceAmount) {
+    snackbar.error('Wähle eine Anzahl für den Würfel');
+    return;
+  }
+  if (!currentDiceRoll.value.diceSides) {
+    snackbar.error('Wähle eine Seitenanzahl für den Würfel');
+    return;
+  }
+  const newDiceRolls = [...model.value.diceRolls, currentDiceRoll.value];
+  model.value = {
+    ...model.value,
+    diceRolls: newDiceRolls as [],
+  };
+  resetDiceRoll();
+  snackbar.success('Würfel hinzugefügt');
+}
+
+function resetDiceRoll() {
+  currentDiceRoll.value = {
+    name: undefined,
+    diceAmount: undefined,
+    diceSides: undefined,
+  };
+}
 
 function addVariable() {
   if (!currentAttribute.value) {
