b1f64d3181e169476acab878fa80188753034351
Simeon Kostadinov <88680364+skostadinov0141@users.noreply.github.com>
2025-07-03 00:16:33 +0200

    add detailed JSDoc comments and new methods for character model management

--- composables/services/api-service.ts
+++ composables/services/api-service.ts
@@ -1,6 +1,11 @@
 import type { $Fetch } from 'nitropack';
 
 export function useApi() {
+  /**
+   * Creates a public client for API requests.
+   * @template T - The type of the response data.
+   * @returns A $Fetch instance configured with the base URL from runtime config.
+   */
   function publicClient<T>(): $Fetch<T> {
     const baseURL = useRuntimeConfig().public.apiBaseUrl;
     return $fetch.create<T>({
--- composables/services/character-model-service.ts
+++ composables/services/character-model-service.ts
@@ -1,6 +1,8 @@
-import type { CreateCharacterModelDto } from '~/composables/dtos/data-contracts';
-
 export function useCharacterModelService() {
+  /**
+   * Creates a new Character Model.
+   * @param payload - The data to create the character model.
+   */
   async function createCharacterModel(payload: CreateCharacterModelDto) {
     return useApi()
       .publicClient<CharacterModel>()('character-model', {
@@ -9,7 +11,41 @@ export function useCharacterModelService() {
       });
   }
 
+  /**
+   * Adds a property to an existing character model.
+   * @param id - The ID of the character model to which the property will be added.
+   * @param payload - The property data to be added to the character model.
+   */
+  async function addPropertyToCharacterModel(
+    id: string,
+    payload: AddPropertyToModelDto,
+  ) {
+    return useApi()
+      .publicClient<CharacterModel>()(`character-model/${id}/properties`, {
+        method: 'POST',
+        body: payload,
+      });
+  }
+
+  /**
+   * Adds a modifier to an existing character model.
+   * @param id - The ID of the character model to which the modifier will be added.
+   * @param payload - The modifier data to be added to the character model.
+   */
+  async function addModifierToCharacterModel(
+    id: string,
+    payload: AddModifierToModelDto,
+  ) {
+    return useApi()
+      .publicClient<CharacterModel>()(`character-model/${id}/modifiers`, {
+        method: 'POST',
+        body: payload,
+      });
+  }
+
   return {
     createCharacterModel,
+    addPropertyToCharacterModel,
+    addModifierToCharacterModel,
   };
 }
